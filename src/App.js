043c35e92b4dfb9594f20033247796e207c59a83
Jose-Casanova <57019113+Jose-Casanova@users.noreply.github.com>
2020-08-27 21:59:03 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="App caja">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+      <div className="App">
+      <h1>Bloque nuevo </h1>
+      <h2>Aprendiendo a entender el react</h2>
+      </div>
+    </>
+  );
+}
